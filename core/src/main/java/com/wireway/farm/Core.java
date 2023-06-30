35a9f030aaab98ba31d1890046787f44396162da
José Zay <josezay22@gmail.com>
2023-06-30 16:47:37 -0300

    add global variables class

--- core/src/main/java/com/wireway/farm/Core.java
+++ core/src/main/java/com/wireway/farm/Core.java
@@ -1,12 +1,19 @@
 package com.wireway.farm;
 
 import com.badlogic.gdx.Game;
+import com.badlogic.gdx.Gdx;
 import com.wireway.farm.screens.GameScreen;
 
 /** {@link com.badlogic.gdx.ApplicationListener} implementation shared by all platforms. */
 public class Core extends Game {
+    Globals globals;
     @Override
     public void create() {
+        globals = new Globals();
         setScreen(new GameScreen());
+    }
+
+    public static Globals getGlobals() {
+        return((Core) Gdx.app.getApplicationListener()).globals; // turn around to not use static objects on Android, neither to pass this around the entire game.
     }
 }
---
+++ core/src/main/java/com/wireway/farm/Globals.java
@@ -0,0 +1,5 @@
+package com.wireway.farm;
+
+public class Globals {
+    public int worldWidht = 300; // The height changes between screen sizes
+}
