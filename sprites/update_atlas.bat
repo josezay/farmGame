a1356266daeaf46d4c9df1e6e78be49dc2fb6fe5
José Zay <josezay22@gmail.com>
2023-06-30 16:11:02 -0300

    add atlas texture packer

---
+++ sprites/update_atlas.bat
@@ -0,0 +1,1 @@
+java -cp runnable-texturepacker.jar com.badlogic.gdx.tools.texturepacker.TexturePacker "imagens" "D:\repositorios\projetos android\ParanaRPG\android\assets" atlas
---
+++ sprites/update_atlas.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+java -cp runnable-texturepacker.jar com.badlogic.gdx.tools.texturepacker.TexturePacker "imagens" "../assets/" atlas
