31155522d30cbf054fa9e951158e593706aa8a25
José Zay <josezay22@gmail.com>
2023-07-05 10:24:07 -0300

    comment globals worldWidht usage

--- core/src/main/java/com/wireway/farm/Globals.java
+++ core/src/main/java/com/wireway/farm/Globals.java
@@ -1,5 +1,5 @@
 package com.wireway.farm;
 
 public class Globals {
-    public final int worldWidht = 300; // The height changes between screen sizes
+    public final int worldWidht = 300; // The height changes accordingly to the device screen ratio.
 }
