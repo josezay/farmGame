9c6b86f54c7aa3414223b1098dad7f3dc026e8bc
José Zay <josezay22@gmail.com>
2023-07-05 10:31:38 -0300

    create base screen to implement commom tasks

---
+++ core/src/main/java/com/wireway/farm/screens/BaseScreen.java
@@ -0,0 +1,48 @@
+package com.wireway.farm.screens;
+
+import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.utils.viewport.Viewport;
+import com.wireway.farm.engine.Graphics;
+
+public class BaseScreen implements Screen {
+    protected Viewport viewport;
+
+    @Override
+    public void show() {
+        initializeScreen();
+    }
+
+    private void initializeScreen() {
+        viewport = Graphics.createExtendViewport();
+    }
+
+    @Override
+    public void render(float delta) {
+        // Draw your screen here. "delta" is the time since last render in seconds.
+    }
+
+    @Override
+    public void resize(int width, int height) {
+        // Resize your screen here. The parameters represent the new window size.
+    }
+
+    @Override
+    public void pause() {
+        // Invoked when your application is paused.
+    }
+
+    @Override
+    public void resume() {
+        // Invoked when your application is resumed after pause.
+    }
+
+    @Override
+    public void hide() {
+        // This method is called when another screen replaces this one.
+    }
+
+    @Override
+    public void dispose() {
+
+    }
+}
