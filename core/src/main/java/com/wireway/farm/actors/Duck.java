8eac655cadd3f49cb722f16d4af4a6c888f51e6e
José Zay <josezay22@gmail.com>
2023-07-07 15:13:59 -0300

    add Duck.java actor class

---
+++ core/src/main/java/com/wireway/farm/actors/Duck.java
@@ -0,0 +1,24 @@
+package com.wireway.farm.actors;
+
+import com.badlogic.gdx.graphics.g2d.Animation;
+import com.badlogic.gdx.graphics.g2d.Batch;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
+import com.badlogic.gdx.scenes.scene2d.Actor;
+import com.wireway.farm.Core;
+
+public class Duck extends Actor {
+    Animation<TextureRegion> idleLeft;
+
+    public Duck() {
+        idleLeft = Core.getGlobals().assets.getAnimation("duck_idle");
+    }
+
+    @Override
+    public void draw(Batch batch, float parentAlpha) {
+        batch.draw(getTextureRegion(), getX(), getY());
+    }
+
+    public TextureRegion getTextureRegion () {
+        return idleLeft.getKeyFrame(Core.getGlobals().elapsedAppTime, true);
+    }
+}
